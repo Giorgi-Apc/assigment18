b0bef6d884332823e518eceb7d5921f33e6e75eb
Giorgi-Apc <goga.mujiri395@ens.tsu.edu.ge>
2025-01-21 19:34:29 +0400

    assigment 18

---
+++ script.js
@@ -0,0 +1,9 @@
+// let result = alert('wrong place');
+//
+// console.log(result);
+
+// let result = confirm('wanna stay?');
+// console.log(result);
+
+let result = prompt('what is your name?');
+console.log(result);
